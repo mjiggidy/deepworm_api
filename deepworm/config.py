330320240e3cafbf6463b5359042d6f8efc627f2
mjordan <michael@glowingpixel.com>
2020-05-18 03:18:56 -0700

    Messy, initial workings

--- deepworm/config.py
+++
@@ -1,6 +0,0 @@
-MYSQL_HOST			= "localhost"
-MYSQL_PORT			= 3306
-MYSQL_USER			= "username"
-MYSQL_PASSWORD		= "password"
-MYSQL_DB			= "db_name"
-MYSQL_CURSORCLASS	= "DictCursor"
---
+++ deepworm/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+	name='deepworm',
+	packages=['deepworm'],
+	include_package_data=True,
+	install_requires=[
+		'flask',
+	],
+)
